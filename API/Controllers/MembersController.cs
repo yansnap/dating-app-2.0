e0536aab39258834faf2fe2adca5e586fcdc5be6
ヤン <jan.kozhyn@gmail.com>
2026-02-27 13:51:18 +0200

    Creating an API controller

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] //localhost:5001/api/members
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IReadOnlyList<AppUser>> GetMembers()
+        {
+            var members = context.Users.ToList();
+
+            return members;
+        }
+
+        [HttpGet("{id}")] //localhost:5001/api/members/boo-id
+        public ActionResult<AppUser> GetMember(string id)
+        {
+            var member = context.Users.Find(id);
+
+            if (member == null) return NotFound();
+
+            return member;
+        }
+
+    }
+}
