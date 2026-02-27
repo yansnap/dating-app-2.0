6a17f664d22e934401c93c241104c42ea1d41a80
ヤン <jan.kozhyn@gmail.com>
2026-02-27 20:20:10 +0200

    Making the code asynchronous

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -10,17 +11,17 @@ namespace API.Controllers
     public class MembersController(AppDbContext context) : ControllerBase
     {
         [HttpGet]
-        public ActionResult<IReadOnlyList<AppUser>> GetMembers()
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
         {
-            var members = context.Users.ToList();
+            var members = await context.Users.ToListAsync();
 
             return members;
         }
 
         [HttpGet("{id}")] //localhost:5001/api/members/boo-id
-        public ActionResult<AppUser> GetMember(string id)
+        public async Task<ActionResult<AppUser>> GetMember(string id)
         {
-            var member = context.Users.Find(id);
+            var member = await context.Users.FindAsync(id);
 
             if (member == null) return NotFound();
 
