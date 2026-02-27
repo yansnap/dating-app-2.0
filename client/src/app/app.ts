aef668a670492934932045ee4f9c9d66abbeac01
ヤン <jan.kozhyn@gmail.com>
2026-02-27 21:24:05 +0200

    Making HTTP requests in Angular

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = 'client';
+export class App implements OnInit{
+  private http = inject(HttpClient);
+  protected readonly title = 'Dating app';
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => console.log('Completed the http request')
+    })
+  }
+
 }
