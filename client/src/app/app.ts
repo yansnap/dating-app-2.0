bc7854c90535cb7a74847fb4734f5d74e9798b83
ヤン <jan.kozhyn@gmail.com>
2026-02-27 21:06:27 +0200

    whats in the angular project

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('client');
+  protected readonly title = 'client';
 }
