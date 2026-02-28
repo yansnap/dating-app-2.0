a6da1e753ab683c008a107d391e8ed1c25085043
ヤン <jan.kozhyn@gmail.com>
2026-02-28 20:29:40 +0200

    Displaying the fetched users in the browser

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit} from '@angular/core';
+import { Component, inject, OnInit, signal} from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,21 @@ import { Component, inject, OnInit} from '@angular/core';
 export class App implements OnInit{
   private http = inject(HttpClient);
   protected readonly title = 'Dating app';
+  protected members = signal<any>([])
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
-      next: response => console.log(response),
-      error: error => console.log(error),
-      complete: () => console.log('Completed the http request')
-    })
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members')) 
+    }catch (error)
+    {
+      console.log(error)
+      throw error
+    }
+    
   }
 
 }
