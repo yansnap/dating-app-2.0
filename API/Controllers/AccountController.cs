7c580684fa97c89ac5d6d3d919a8e39a5726e13e
yansnap <jan.kozhyn@gmail.com>
2026-05-18 19:51:46 +0300

    Creating a RegisterDTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,24 +1,27 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(AppDbContext context) : BaseApiController
 {
     [HttpPost("register")] // api/account/register
-    public async Task<ActionResult<AppUser>> Register(string email, string displayName, 
-        string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDto)
     {
+        if (await EmailExists(registerDto.Email)) return BadRequest("Email is already taken");
+
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
-            DisplayName = displayName,
-            Email = email,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -26,5 +29,10 @@ public class AccountController(AppDbContext context) : BaseApiController
         await context.SaveChangesAsync();
 
         return user;
+    }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
     }
 }
---
+++ API/DTOs/RegisterDTO.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDTO
+{
+    [Required]
+    public string DisplayName { get; set; } = "";
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = "";
+    [Required]
+    [MinLength(4)]
+    public string Password { get; set; } = "";
+}
