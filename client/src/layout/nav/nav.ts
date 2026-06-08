2e34a204633da268d54d309b25624a419b17e66a
yansnap <jan.kozhyn@gmail.com>
2026-06-08 23:10:13 +0300

    Using template forms in Angular

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {}
+export class Nav {
+  protected creds: any = {};
+
+  login() {
+    console.log(this.creds);
+  }
+}
