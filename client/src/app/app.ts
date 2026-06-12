a281f13b771f1b0da3cd2b7c65f72a13163fc3ea
yansnap <jan.kozhyn@gmail.com>
2026-06-12 18:20:22 +0300

    Persisting the login with signals part 2

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal} from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from '../layout/nav/nav';
+import { AccountService } from '../core/services/account-service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,21 @@ import { Nav } from '../layout/nav/nav';
   styleUrl: './app.css'
 })
 export class App implements OnInit{
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected readonly title = 'Dating app';
   protected members = signal<any>([])
 
   async ngOnInit() {
     this.members.set(await this.getMembers())
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 
   async getMembers() {
