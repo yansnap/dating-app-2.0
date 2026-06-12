6de7bb0bb83fa9293a0a8079ba8a11abc3231854
yansnap <jan.kozhyn@gmail.com>
2026-06-12 19:03:26 +0300

    Adding a register form

--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { Register } from "../account/register/register";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Register],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
